9547541eb53529072f75c4cfd842ddb2753f181d
Sofia Morales <sofimoralesroa92@gmail.com>
2025-02-25 16:16:49 +0100

    Word model

---
+++ models/Word.model.js
@@ -0,0 +1,36 @@
+const { Schema, model } = require("mongoose");
+
+const wordSchema = new Schema({
+  word: {
+    type: String,
+    required: [true, "word is required."],
+  },
+
+  meaning: {
+    type: String,
+    required: [true, "Meaning is required."],
+  },
+
+  translation: {
+    type: String,
+    required: [true, "Translation is required."],
+  },
+
+  sentences: {
+    type: Array,
+    required: [true, "Sentences is required."],
+  },
+
+  language: {
+    type: String,
+    required: [true, "Lenguage is required."],
+  },
+  user_id: {
+    type: ObjectId,
+    required: [true, "user_id is required."],
+  },
+});
+
+const Word = model("Word", wordSchema);
+
+module.exports = Word;
