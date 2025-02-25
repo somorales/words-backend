2b747471e2696be909ffc1e69520dd6d046cac42
Sofia Morales <sofimoralesroa92@gmail.com>
2025-02-25 21:47:23 +0100

    rutas DELETE, PUT, y POST

--- routes/word.routes.js
+++ routes/word.routes.js
@@ -9,9 +9,9 @@ router.post("/", async (req, res, next) => {
     const response = await Word.create({
       word: req.body.word,
       meaning: req.body.meaning,
-      meaning: req.body.meaning,
       translation: req.body.translation,
       sentences: req.body.sentences,
+      language: req.body.language,
     });
 
     res.status(201).json(response);
@@ -28,6 +28,44 @@ router.get("/", async (req, res, next) => {
     const allWords = await Word.find();
 
     res.status(200).json(allWords);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+//ver una palabra
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const word = await Word.findById(req.params.id);
+    res.status(200).json(word);
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+//eliminar una palabra
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const response = await Word.findByIdAndDelete(req.params.id);
+
+    res.status(204).send();
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
+});
+
+//editar una palabra
+router.put("/:id", async (req, res, next) => {
+  try {
+    const updateWord = await Word.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+    });
+
+    res.status(200).json(updateWord);
   } catch (error) {
     console.log(error);
     next(error);
